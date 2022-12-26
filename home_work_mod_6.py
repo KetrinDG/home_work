fa058a563b8da01f10be2cf1b7244a7c0bcb0c3d
KetrinDG <degtyareva.ev1@gmail.com>
2022-12-26 20:51:27 +0200

    update

--- home_work_mod_6.py
+++ home_work_mod_6.py
@@ -1,10 +1,11 @@
 import shutil
-import os #для получения информации о файлах
+import os
+from pathlib import Path
+from typing import List
 
-path = input('Input folder path: ')  # папка сортировки
 
 # Ключи - названия папок. Значения - расширения файлов для каждой отдельной папки.
-extensions = {
+CATEGORIES = {
     'video': ['mp4', 'mov', 'avi', 'mkv', 'wmv', '3gp', '3g2', 'mpg', 'mpeg', 'm4v',
               'h264', 'flv', 'rm', 'swf', 'vob'],
     'data': ['sql', 'sqlite', 'sqlite3', 'csv', 'dat', 'db', 'log', 'mdb', 'sav',
@@ -25,11 +26,22 @@ extensions = {
     'apk': ['apk']
 }
 
+path = input('Input folder path: ')
+
+def main():
+    create_folders_from_list(path, CATEGORIES)
+    get_subfolder_paths(path)
+    get_file_paths(path)
+    sort_files(path)
+    remove_empty_folders(path)
+
+
 # Напишем функцию для создания папок из списка названий
 def create_folders_from_list(folder_path, folder_names):
+    os.mkdir(os.path.join(folder_path, 'unknowns'))
     for folder in folder_names:
-        if not os.path.exists(f'{folder_path}\\{folder}'):
-            os.mkdir(f'{folder_path}\\{folder}')
+        if not os.path.exists(os.path.join(folder_path, folder)):
+            os.mkdir(os.path.join(folder_path, folder))
 
 # для получения путей подпапок
 def get_subfolder_paths(folder_path) -> list:
@@ -44,21 +56,24 @@ def get_file_paths(folder_path) -> list:
 # сортировка файлов
 def sort_files(folder_path):
     file_paths = get_file_paths(folder_path)  # пути файлов
-    ext_list = list(extensions.items())  # cоздаем переменную со списком метода словаря
-    # цикл для каждого пути файла в списке
+    file_path: os.path
     for file_path in file_paths:
-        extension = file_path.split('.')[-1]
-        file_name = file_path.split('\\')[-1]
-        shutil.unpack_archive(file_path, 'new_folder_for_data')
+        moved = False
+        file = Path(file_path)
+        extension = file.suffix.replace('.', '')
+        file_name = file.name
+        for f in Path.home().iterdir():
+            print(f.name)
         # цикл внутри
-        for dict_key_int in range(len(ext_list)):
-            if extension in ext_list[dict_key_int][1].lower:
-                print(f'Moving {file_name} in {ext_list[dict_key_int][0].lower} folder\n')
-                os.rename(file_path, f'{path}\\{ext_list[dict_key_int][0].lower}\\{file_name}')
-            else:
-                file = path + "/" + items
-                n_path = "/unknown/" + items
-                shutil.move(file, n_path)
+        for category, extensions in CATEGORIES.items():
+            if extension in extensions:
+                print(f'Moving {file_path} in {category} folder\n')
+                os.rename(file_path, os.path.join(path, category,  file_name))
+                moved = True
+                break
+        if not moved:
+            file_destination = os.path.join(folder_path, 'unknowns')
+            os.rename(file_path, os.path.join(file_destination, file_name ))
 
 # удаляем пустые папки
 def remove_empty_folders(folder_path):
@@ -68,9 +83,7 @@ def remove_empty_folders(folder_path):
             print('Deleting empty folder:', p.split('\\')[-1], '\n')
             os.rmdir(p)
 
+
 if __name__ == "__main__":
-    create_folders_from_list(path, extensions)
-    get_subfolder_paths(path)
-    get_file_paths(path)
-    sort_files(path)
-    remove_empty_folders(path)
+    main()
+
